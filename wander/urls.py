ec23fcd45b55c71b858fc34f3ea762b1d3452f2a
Danya <danimen0001@gmail.com>
2020-12-01 15:11:10 +0300

    add templates

--- wander/urls.py
+++ wander/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path
 
-from wander.views import PosterEventListView, PosterEventDetailView, RestaurantListView, RestaurantDetailView
+from wander.views import PosterEventListView, PosterEventDetailView, RestaurantListView, RestaurantDetailView, AttractionListView, AttractionDetailView
 
 urlpatterns = [
     path('', PosterEventListView.as_view(), name='poster-list'),
+    path('poster/', PosterEventListView.as_view(), name='poster-list'),
     path('poster/<int:pk>/', PosterEventDetailView.as_view(), name='poster-detail'),
-    # path('favourites', views.favourites, name='favourites'),
-    path('posters', PosterEventListView.as_view(), name='poster-list'),
-    # path('posters/<int:pk>', views.PosterEventListView.as_view(), name='posters'),
-    # path('rating', views.rating, name='rating'),
+    path('attraction/', AttractionListView.as_view(), name='attraction-list'),
+    path('attraction/<int:pk>/', AttractionDetailView.as_view(), name='attraction-detail'),
     path('restaurant/', RestaurantListView.as_view(), name='restaurant-list'),
     path('restaurant/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    # path('rating', views.rating, name='rating'),
     # path('settings', views.settings, name='settings'),
-    # path('', views.posters, name='posters'),
-    # path('posters', views.posters, name='posters'),
 ]
 
 
