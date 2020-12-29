c539793a62f3c311bea00486eaba418d10162846
Danya <danimen0001@gmail.com>
2020-12-29 09:37:53 +0300

    render

--- wander/urls.py
+++ wander/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', home, name='homepage'),
     path('poster/', PosterEventListView.as_view(), name='poster-list'),
     path('poster/<int:pk>/', PosterEventDetailView.as_view(), name='poster-detail'),
-    # path('poster/<slug:poster>/', poster_single, name='poster-detail'),
     path('attraction/', AttractionListView.as_view(), name='attraction-list'),
     path('attraction/<int:pk>/', AttractionDetailView.as_view(), name='attraction-detail'),
     path('restaurant/', RestaurantListView.as_view(), name='restaurant-list'),
